41f26328ffe2b69ba4fa6a1ce295558c59fc6b26
Martin <wienc.martin@gmail.com>
2023-01-09 22:45:17 -0500

    added auth0 provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { config } from './utils/config'
+import * as dotenv from 'dotenv'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App/>
+    <Auth0Provider
+      domain={config().domain}
+      clientId={config().clientId}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 )
