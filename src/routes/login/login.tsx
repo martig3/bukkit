305e178784670888bfa910b1cba0417693eb6d08
Martin <wienc.martin@gmail.com>
2023-12-27 12:16:58 -0500

    improve login

--- src/routes/login/login.tsx
+++ src/routes/login/login.tsx
@@ -1,4 +1,13 @@
-import { AppShell, Button, Card, Center, Flex } from "@mantine/core";
+import {
+  AppShell,
+  Avatar,
+  Button,
+  Card,
+  Center,
+  Flex,
+  Title,
+  Text,
+} from "@mantine/core";
 import { config } from "../../utils/config";
 import { IconBrandDiscord } from "@tabler/icons-react";
 export function Login() {
@@ -12,15 +21,24 @@ export function Login() {
       })}
     >
       <Center maw={400} h={"100%"} mx="auto">
-        <Card px={32} pb={64} w={300}>
-          <h1>Login</h1>
+        <Card p={"xl"} w={300}>
+          <Flex dir="row" mb={16}>
+            <Avatar
+              bg={"orange"}
+              radius={"xl"}
+              mx={8}
+              src={"/android-chrome-192x192.png"}
+            />
+            <Title order={2}>Bukkit</Title>
+          </Flex>
           <Flex justify={"center"}>
             <Button
               leftIcon={<IconBrandDiscord />}
               onClick={async () => await login()}
-              w={"100%"}
+              miw={"fit-content"}
+              maw={"100%"}
             >
-              Discord
+              Continue with Discord
             </Button>
           </Flex>
         </Card>
